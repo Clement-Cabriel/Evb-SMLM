3954b8717f59a35bb4fa6db653d12169e63715af
Clément Cabriel <109362300+Clement-Cabriel@users.noreply.github.com>
2025-11-25 19:06:28 +0100

    Update Read_data_evb.py

--- Read_data_evb.py
+++ Read_data_evb.py
@@ -41,7 +41,7 @@ from metavision_core.event_io.raw_reader import RawReader
 #-----------------------------------------------------------------------------
 
 # Import parameters
-filepath=''
+filepath='C:/absolute/path/to/data/AF647_coverslip.raw'
 buffer_size=1e8                 # increase the buffer size if the input file is too large to be loaded. Decrease the buffer size if the PC is out of memory. Tested successfully with buffer size = 4e9 for 128 Gb of memory
 frame_size=[1280,720]           # Size of the sensor array (in pixels) [x,y]. =[480,640] for the gen3; =[1280,720] for the gen4
 
@@ -208,4 +208,5 @@ if export_frames:
     else:Image.fromarray(frames[:,:,0]).save(filepath[:-4]+'_frames_sum_'+sign_display+'.tif')
 print('Frames generation done')
 print('')
+
 
